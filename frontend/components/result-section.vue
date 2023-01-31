fc8595fdd008abc2cbfaa164ef04ad72c898744d
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2023-01-31 11:25:08 +0000

    feat: :sparkles: Added analytics section, changed UI layout

--- frontend/components/result-section.vue
+++ frontend/components/result-section.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="text-background text-center grid grid-cols-5 auto-rows-max gap-4 h-screen w-screen">
-        <single-container class="col-start-2 row-start-5" title="Total Messages" :value="data.total_messages"/>
-        <single-container class="col-start-3 row-start-5" title="Total Channels" :value="data.total_channels"/>
+    <div class="text-background text-center grid grid-cols-5 auto-rows-max gap-4 w-screen mb-8 ">
+        <BarChart class="col-start-2 row-start-3 col-span-3" :data="data" />
+        <BrowserChart class="col-start-2 row-start-4 col-span-2" :data="data" />
 
-        <list-container class="col-start-2 row-start-6 col-span-1" title="Top Friends" :list="data.friends"/>
+        <div class="col-start-4 col-span-1 row-start-4 flex flex-col gap-4">
+          <single-container class="col-start-2 row-start-5" title="Total Messages" :value="data.total_messages"/>
+          <single-container class="col-start-3 row-start-5" title="Calls Started" :value="data.events['start_call']"/>
+          <single-container class="col-start-4 row-start-5" title="Voice Channels Joined" :value="data.events['join_voice_channel']"/>
+        </div>
+        
+        <!-- <list-container class="col-start-2 row-start-6 col-span-1" title="Top Friends" :list="data.friends"/>
         <list-container class="col-start-3 row-start-6 col-span-1" title="Top Servers" :list="data.servers"/>
-        <list-container class="col-start-4 row-start-6 col-span-1" title="Top Groups" :list="data.groups"/>
+        <list-container class="col-start-4 row-start-6 col-span-1" title="Top Groups" :list="data.groups"/> -->
 
-        <BarChart class="col-start-2 row-start-7 col-span-3" :data="data"/>
+        <Analytics class="col-start-2 row-start-5 col-span-3" :data="data.events" />
 
-        <!-- <BarChart class="col-start-2 row-start-7 col-span-3" :data="data.friends"/> -->
+        <!-- <Toggle /> -->
+
     </div>
 </template>
 
